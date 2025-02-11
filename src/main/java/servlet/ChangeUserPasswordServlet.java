9aeef8c9cc90c7e30026bc11bf77097fb37e61d2
23JZ0104 <23jz0104@jec.ac.jp>
2025-02-11 13:00:51 +0900

    actionパラメータで遷移先作成

--- src/main/java/servlet/ChangeUserPasswordServlet.java
+++ src/main/java/servlet/ChangeUserPasswordServlet.java
@@ -49,13 +49,31 @@ public class ChangeUserPasswordServlet extends HttpServlet {
             return;
         }
 
+        String action = request.getParameter("action");
+
+        if ("passwordConfirm".equals(action)) {
+            url = passwordConfirm(request);
+            Logic.forward(request, response, url);
+        }
+        else if ("changePassword".equals(action)) {
+            url = changePassword(request);
+            Logic.forward(request, response, url);
+        }
+
+    }
+
+    /**
+     * パスワード確認要処理
+     * 
+     * @param  request リクエスト
+     * @return         遷移先URL
+     */
+    private String passwordConfirm(HttpServletRequest request) {
         String password = request.getParameter("password");
 
         if (Logic.hasEmptyValues(password)) { //空文字判定
             request.setAttribute("message", "パスワードが入力されていません。");
-            url = "WEB-INF/jsp/changeUserPasswordConfirm.jsp";
-            Logic.forward(request, response, url);
-            return;
+            return "WEB-INF/jsp/changeUserPasswordConfirm.jsp";
         }
 
         HttpSession session = request.getSession();
@@ -65,9 +83,50 @@ public class ChangeUserPasswordServlet extends HttpServlet {
         user = userDAO.getUserByIdAndPassword(userId, password);
 
         if (user != null) { //ユーザーが見つかってれば
+            return "WEB-INF/jsp/changeUserPassword.jsp";
 
         }
+        else { //ユーザーが見つからなければ
+            request.setAttribute("message", "パスワードが間違っています。");
+            return "WEB-INF/jsp/changeUserPasswordConfirm.jsp";
+        }
+    }
 
+    /**
+     * パスワード更新処理
+     * 
+     * @param  request リクエスト
+     * @return         遷移先URL
+     */
+    private String changePassword(HttpServletRequest request) {
+        String password        = request.getParameter("password");
+        String confirmPassword = request.getParameter("confirmPassword");
+
+        if (Logic.hasEmptyValues(password, confirmPassword)) {
+            request.setAttribute("message", "入力内容が不足しています。");
+            return "WEB-INF/jsp/changePassword";
+        }
+
+        if (password.equals(confirmPassword)) { //パスワードが一致していれば
+            HttpSession session = request.getSession();
+            User        user    = (User)session.getAttribute("user");
+            int         userId  = user.getId();
+            UserDAO     userDAO = new UserDAO();
+
+            if (userDAO.updateUserPassword(userId, password)) { //パスワードの更新に成功したら
+                user = userDAO.getUserById(userId);
+                session.setAttribute("user", user);
+                return "WEB-INF/jsp/changePasswordComplete.jsp";
+            }
+            else {
+                request.setAttribute("message", "パスワードの更新に失敗しました。");
+                return "WEB-INF/jsp/changePassword";
+            }
+        }
+        else {
+            request.setAttribute("message", "パスワードが一致しません");
+            return "WEB-INF/jsp/changePassword";
+        }
     }
 
 }
